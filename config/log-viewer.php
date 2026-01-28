23cb80dd1ff3b5ede9bc0e7b8730ac4785653987
dnysaz <danayasa2@gmail.com>
2026-01-28 13:07:23 +0800

    update logs

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -98,7 +98,7 @@ return [
     'middleware' => [
         'web',
         'auth',
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        // \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
         \App\Http\Middleware\OnlyAdminCanSeeLogs::class,
     ],
 
